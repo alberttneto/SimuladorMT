d21e874b2c06ff967217619e7b3d4ed1c9a48085
alberttneto <bf__@hotmail.com>
2022-05-31 12:33:56 -0300

    Algoritmo da MT

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
-var texto;
+var texto, palavra;
 
 function readSingleFile(evt) {
-  //Retrieve the first (and only!) File from the FileList object
+
   var f = evt.target.files[0]; 
   console.log(evt);
   console.log("teste")
@@ -22,22 +22,86 @@ document.getElementById('fileinput').addEventListener('change', readSingleFile, 
 
 function exibirMt(){
   var input = document.querySelector("#palavra");
-  var palavra = input.value;
+  var cont = 0;
+  palavra = input.value;
 
   console.log(palavra);
 
   var ul = document.getElementById("fita");
 
+  for (child of ul.children){
+    child.remove();
+  }
+
   for (elemento of palavra){
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(elemento));
-    li.setAttribute("id", "element4"); // added line
+    li.setAttribute("id", cont);
     ul.appendChild(li);
-  }
+    cont++;
+  } 
 
+  executaMT();
 }
-
 
 function exibirTexto(){
-  console.log(texto);
+  const obj = JSON.parse(texto);
+  console.log(obj);
 }
+
+function transicaoSaida(obj, estado, proxElemento){
+  for (var [key,value] of Object.entries(obj["Transicao"])){
+      var entrada = key.split("-")
+      var saida = value.split(",")
+      //console.log(saida)
+      if (entrada[0] == estado && entrada[1] == proxElemento){
+          //estado = saida[0];
+          //palavra[pos] = saida[1];
+          return saida;
+      }
+  }
+  return "erro"
+}
+
+function executaMT(){
+  fita = palavra.split("");
+  const obj = JSON.parse(texto);
+  var estado = obj["EstadoInicial"], pos = 0;
+  var proxElemento = "";
+  console.log(estado);
+  console.log(fita)
+  // Andar pelo vetor
+  while(1){
+    proxElemento = fita[pos];
+    
+    console.log(celula);
+    
+    console.log(estado);
+    console.log(proxElemento);
+    var saida = transicaoSaida(obj, estado, proxElemento);
+    if (saida == "erro"){
+        console.log("Palavra não aceita")
+        break;
+    }
+    console.log(saida)
+    estado = saida[0];
+    fita[pos] = saida[1];
+    console.log(fita)
+    
+    var celula = document.getElementById(pos);
+    celula.classList.toggle("destacaCelula");
+    if (saida[2] == 'R'){
+        pos++;
+    } else{
+        pos--;
+    }
+    celula = document.getElementById(pos);
+    //celula.classList.toggle("destacaCelula");
+
+    if (obj["EstadosFinais"].indexOf(estado) > -1){
+        console.log("Palavra aceita");
+        break;
+    }
+  }
+}
+
